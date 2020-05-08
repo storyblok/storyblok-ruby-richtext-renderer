170bfe51a255b535ffbce0741f15706bc8bef662
ademarCardoso <ac@storyblok.com>
2020-05-08 14:57:51 -0300

    added test to anchor link

--- spec/richtext_spec.rb
+++ spec/richtext_spec.rb
@@ -26,3 +26,31 @@ describe 'richtext' do
     expect(renderer.render(doc)).to eq('<span class="red">red text</span>')
   end
 end
+
+describe 'richtext' do
+  it 'anchor links test' do
+    doc = {
+      'type' => 'doc',
+      'content' => [
+      {
+        'text' => 'link text',
+        'type' => 'text',
+        'marks' => [
+          {
+            'type' => 'link',
+            'attrs' => {
+              'href' => '/link',
+              'target' => '_blank',
+              'uuid' => '300aeadc-c82d-4529-9484-f3f8f09cf9f5',
+              'anchor' => 'anchor-text'
+            }
+          }
+        ]
+      }
+    ]
+    }
+
+    renderer = Storyblok::Richtext::HtmlRenderer.new
+    expect(renderer.render(doc)).to eq('<a href="/link#anchor-text" target="_blank" uuid="300aeadc-c82d-4529-9484-f3f8f09cf9f5">link text</a>')
+  end
+end
